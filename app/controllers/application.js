8e9890f42f761f5b3b6af558376de488d383ecab
Karin Fernandez <karinfdez@yahoo.com>
2016-06-23 12:07:38 -0400

    query params and link-to

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	
+	// This queryParams is taking whatever value I'm passing in the URL
+	//for example: http://localhost:4200/?student=Erik
+	queryParams: ['student'],
+	student: null
 });
